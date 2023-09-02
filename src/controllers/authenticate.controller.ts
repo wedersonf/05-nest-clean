9686890859c438a96ed16a9680c81c53ed71a7a7
wedersonf <wedymf@hotmail.com>
2023-09-02 16:32:18 -0300

    feat: replace import paths

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -7,8 +7,8 @@ import {
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { z } from 'zod'
-import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
-import { PrismaService } from 'src/prisma/prisma.service'
+import { ZodValidationPipe } from '@/pipes/zod-validation-pipe'
+import { PrismaService } from '@/prisma/prisma.service'
 import { compare } from 'bcryptjs'
 
 const authenticateBodySchema = z.object({
