7036ec0a20c61fe4f538e11f70ffaa903fad08c2
wedersonf <wedymf@hotmail.com>
2023-09-10 05:43:18 -0300

    refactor: return left when throw error

--- src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,13 +1,14 @@
-import { Either, right } from '@/core/either'
+import { Either, left, right } from '@/core/either'
 import { Question } from '../../enterprise/entities/question'
 import { QuestionsRepository } from '../repositories/questions-repository'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
 }
 
 type GetQuestionBySlugUseCaseResponse = Either<
-  null,
+  ResourceNotFoundError,
   {
     question: Question
   }
@@ -22,7 +23,7 @@ export class GetQuestionBySlugUseCase {
     const question = await this.questionsRepository.findBySlug(slug)
 
     if (!question) {
-      throw new Error('Question not found.')
+      return left(new ResourceNotFoundError())
     }
 
     return right({
